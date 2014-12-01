639c5ab6c6dd555972672ac325081b7535e2c739
wertion <wertion@users.noreply.github.com>
2014-12-01 22:30:38 +0800

    Update travis-tool.sh

--- scripts/travis-tool.sh
+++ scripts/travis-tool.sh
@@ -70,7 +70,7 @@ BootstrapLinux() {
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update -qq
     sudo apt-get install -qq gcc-4.9
-    apt-get install -y --no-install-recommends \
+    sudo apt-get install -y --no-install-recommends \
     bash-completion \
     bison \
     debhelper \
