ae2e61ec3383bd323c76e7345e07fdfc1089d7e7
wertion <wertion@users.noreply.github.com>
2014-12-13 21:10:27 +0800

    Update travis-tool.sh

--- scripts/travis-tool.sh
+++ scripts/travis-tool.sh
@@ -145,7 +145,7 @@ BootstrapLinux() {
     # but that may affect only the next shell
     sudo chmod 2777 /usr/local/lib/R /usr/local/lib/R/site-library
     mkdir ~/.R 
-    sudo sh -c "echo -e 'CC = clang -std=gnu99 -fsanitize=undefined -fno-omit-frame-pointer\nCXX = clang++ -fsanitize=undefined -fno-omit-frame-pointer\nPKG_LIBS = /usr/lib/llvm-3.4/lib/clang/3.4/lib/linux/libclang_rt.ubsan_cxx-x86_64.a'  > ~/.R/Makevars" 
+    echo -e "CC = clang -std=gnu99 -fsanitize=undefined -fno-omit-frame-pointer\nCXX = clang++ -fsanitize=undefined -fno-omit-frame-pointer\nPKG_LIBS = /usr/lib/llvm-3.4/lib/clang/3.4/lib/linux/libclang_rt.ubsan_cxx-x86_64.a"  > ~/.R/Makevars 
 
 
     # Process options
